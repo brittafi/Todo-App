4cacaa4d5ab9aa33d840d048b541c44c861b127e
ChEb <christiana.eberle@mni.thm.de>
2020-11-19 13:43:38 +0100

    init firebase connection

--- todo-app/src/app/app.module.ts
+++ todo-app/src/app/app.module.ts
@@ -4,13 +4,28 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDtE_VpAXduml53_SPDVMk-Oghe_X-kPAE',
+  authDomain: 'kms-todolist.firebaseapp.com',
+  databaseURL: 'https://kms-todolist.firebaseio.com',
+  projectId: 'kms-todolist',
+  storageBucket: 'kms-todolist.appspot.com',
+  messagingSenderId: '661455319693',
+  appId: '1:661455319693:web:54a909fe4a11fd7ba3a8c4'
+};
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
