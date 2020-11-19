7077a968b1c042a4738c0b3e2ccc7a44cf53b3f2
ChEb <christiana.eberle@mni.thm.de>
2020-11-19 13:48:14 +0100

    add listener to check if firebase connection works

--- todo-app/src/app/app.component.ts
+++ todo-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(private db: AngularFirestore) {
+    // todo remove; just for testing
+    const things = db.collection('users').valueChanges();
+    things.subscribe(console.log);
+    // *****
+  }
   title = 'todo-app';
 }
