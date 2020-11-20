5f549d2b9726c963d8429fff3f7589faa22c5ec0
bift85 <britta.fichtl@mni.thm.de>
2020-11-20 14:52:02 +0100

    started confirmation

--- todo-app/src/app/add-task/add-task.component.ts
+++ todo-app/src/app/add-task/add-task.component.ts
@@ -8,10 +8,24 @@ import {TaskService} from '../task.service';
   styleUrls: ['./add-task.component.css']
 })
 export class AddTaskComponent implements OnInit {
+
   newTask: Task;
+
   constructor(private taskService: TaskService) { }
 
   ngOnInit() {
+    this.newTask = {
+      title: '',
+      description: '',
+      priority: 3
+    };
   }
 
+  cancel() {
+
+  }
+
+  confirm(title: string, description: string, priority: number){
+    console.log(this.newTask.priority.toString());
+  }
 }
