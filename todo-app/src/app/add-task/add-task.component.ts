b8bb34c409e311911625efc523494424f058ce5b
Pia-Doreen Ritzke <pia-doreen.ritzke@mni.thm.de>
2020-11-20 15:24:36 +0100

    Adds method for task editing.

--- todo-app/src/app/add-task/add-task.component.ts
+++ todo-app/src/app/add-task/add-task.component.ts
@@ -31,22 +31,36 @@ export class AddTaskComponent implements OnInit {
   }
 
   confirm() {
-    if(this.newTask.title.length == 0 ){
-      alert("Please add a title.")
-      return;
+    if (this.newTask.title.length === 0) {
+      this.showWarning();
+    } else {
+      this.taskService.addTask('cebr76', this.newTask); // TODO: get real user name when implementing user registration
+      console.log(this.taskService.getAllTasks('cebr76')); // only for debugging
+      this.resetTask();
+      document.getElementById('app-add-task').style.display = 'none';
+      document.getElementById('btn-add-task').style.display = 'block';
     }
-    this.taskService.addTask('cebr76', this.newTask);
-    console.log(this.taskService.getAllTasks("cebr76"));
-    this.resetTask();
-    document.getElementById('app-add-task').style.display = 'none';
-    document.getElementById('btn-add-task').style.display = 'block';
-
   }
 
-  resetTask(){
+  resetTask() {
     this.newTask.title = '';
     this.newTask.description = '';
     this.newTask.priority = 3;
   }
 
+  // TODO: call from task list with task to be edited
+  editTask(task: Task) {
+    this.newTask = task;
+    if (this.newTask.title === '') {
+      this.showWarning();
+    } else {
+      this.taskService.updateTask('cebr76', this.newTask); // TODO: get real user name when implementing user registration
+    }
+  }
+
+  showWarning() {
+    alert('Please add a title.');
+  }
 }
+
+
