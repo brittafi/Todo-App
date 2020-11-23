0268c753cc02eea4eae01f52a701816d9d49452d
Dxree <pia-doreen.ritzke@mni.thm.de>
2020-11-23 16:14:30 +0100

    Fixes deleted files.

--- todo-app/src/app/app.module.ts
+++ todo-app/src/app/app.module.ts
@@ -7,7 +7,6 @@ import { AppComponent } from './app.component';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { GetListComponent } from './get-list/get-list.component';
-import { TaskComponent } from './task/task.component';
 import {FormsModule} from '@angular/forms';
 import { AddTaskComponent } from './add-task/add-task.component';
 
@@ -26,7 +25,6 @@ const firebaseConfig = {
     AppComponent,
     GetListComponent,
     AppComponent,
-    TaskComponent,
     AddTaskComponent
   ],
   imports: [
